c2c483b0e80080969c7cbbe87b947a66b52591e8
linzz <breakitwall@gmail.com>
2014-11-01 11:16:56 +0800

    11-01

---
+++ quick_sort.py
@@ -0,0 +1,14 @@
+def some_fun():
+    pass
+
+
+def quick_sort(list, s, l):
+    pass
+
+from random import shuffle
+x = [i for i in range(10)]
+
+for z in xrange(1, 10):
+    shuffle(x)
+    quick_sort(x, 0, len(x))
+    print x
