2ec17e583881e1c59ed6686c64fae902d31498c9
AnewG <breakitwall@gmail.com>
2015-08-03 10:25:30 +0800

    Power of Two

---
+++ leetcode/Power_of_Two.js
@@ -0,0 +1,11 @@
+/**
+ * Target
+ *  Given an integer, write a function to determine if it is a power of two.
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+   
+    return (n > 0) && (!(n & (n - 1)));
+ 
+};
