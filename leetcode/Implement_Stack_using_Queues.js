a8c0202a1c89cf196228ad51004a1559e4882e5a
AnewG <breakitwall@gmail.com>
2015-08-05 15:33:51 +0800

    Implement Stack using Queues

---
+++ leetcode/Implement_Stack_using_Queues.js
@@ -0,0 +1,39 @@
+/**
+ * @constructor
+ */
+var Stack = function() {
+    this.stack = [];
+};
+
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+Stack.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * @returns {void}
+ */
+Stack.prototype.pop = function() {
+    this.stack.pop();
+};
+
+/**
+ * @returns {number}
+ */
+Stack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @returns {boolean}
+ */
+Stack.prototype.empty = function() {
+    if(this.stack.length === 0){
+        return true;
+    }else{
+        return false;
+    }
+};
