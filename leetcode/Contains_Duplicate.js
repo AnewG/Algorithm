57d18bae53313407d7b77b95174c60a2d13c2f16
AnewG <breakitwall@gmail.com>
2015-08-05 17:30:24 +0800

    Contains Duplicate

---
+++ leetcode/Contains_Duplicate.js
@@ -0,0 +1,18 @@
+/**
+ * Target
+ *   Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    var map = [];
+    var len = nums.length;
+    for(var i=0; i<len; i++){
+        if(typeof map[nums[i]] == 'undefined'){
+            map[nums[i]] = 1;
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
