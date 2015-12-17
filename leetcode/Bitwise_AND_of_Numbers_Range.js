94d9038c731396cfd6f08abb81794ca722768bca
AnewG <breakitwall@gmail.com>
2015-12-17 10:14:28 +0800

    Bitwise AND of Numbers Range
    Bitwise AND of Numbers Range

---
+++ leetcode/Bitwise_AND_of_Numbers_Range.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+ 
+var rangeBitwiseAnd = function(m, n) {
+    while(n > m){
+        n = n & (n-1);
+    }
+    
+    return m & n;
+};
